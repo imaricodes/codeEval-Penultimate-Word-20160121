2185981dce0972aa03153a4ba1517b6f1c8af3df
imaricodes <imaricodes@gmail.com>
2016-01-21 14:05:04 -0500

    first commit. challenge solved.

---
+++ codeEval-Penultimate Word-20160121/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codeEval_Penultimate_Word_20160121
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            //**Code Eval Directions: Write a program which finds the next-to-last word in a string.
+            //**Solved
+
+            string line = "Betty June Sally Jesus Mary Joseph Carlos";
+
+            string[] input = line.Split(' ');
+            Console.WriteLine(input[input.Length - 2]);
+
+            
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
